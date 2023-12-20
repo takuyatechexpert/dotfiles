545dc183224e4c3d2dcb7ffc49d51d26b38a6e19
furukawa <takuya.furukawa@qnote.co.jp>
2023-12-20 14:13:55 +0900

    fix: add telescope hidden file find command

--- packages/neovim/.config/nvim/lua/config/telescope.lua
+++ packages/neovim/.config/nvim/lua/config/telescope.lua
@@ -2,6 +2,7 @@ local opts = { noremap=true, silent=true }
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, opts)
+vim.keymap.set('n', '<leader>fa', '<CMD>Telescope find_files hidden=true <CR>', opts)
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, opts)
 vim.keymap.set('n', '<leader>fb', builtin.buffers, opts)
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, opts)
