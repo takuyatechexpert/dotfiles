e708884e73f1346a90578b3f16fdea3864154e47
furukawa <takuya.furukawa@qnote.co.jp>
2024-01-01 17:15:27 +0900

    feat: terminal scroll by line is key bindings

--- packages/wezterm/.wezterm.lua
+++ packages/wezterm/.wezterm.lua
@@ -3,8 +3,13 @@ local act = wezterm.action
 local config = {}
 
 config.keys = {
+  -- move tab
   { key = 'p', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
   { key = 'n', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
+
+  -- ScrollByPage
+  { key = 'y', mods = 'CTRL|SHIFT', action = act.ScrollByLine(-1) },
+  { key = 'e', mods = 'CTRL|SHIFT', action = act.ScrollByLine(1) },
 }
 
 return config
